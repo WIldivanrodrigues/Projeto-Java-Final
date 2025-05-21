f5caa6699ead65a03675fd47c3010064c9aaac28
Wildivan Rodrigues <williurd23@gmail.com>
2025-05-20 21:53:43 -0300

    "Criação classes Main e CadastroUser"

--- src/model/CadastroUser.java
+++ src/model/CadastroUser.java
@@ -1,3 +1,48 @@
 package model;
 
-public class CadastroUser {}
+public class CadastroUser {
+
+    private String Nome;
+    private String DataNascimento;
+    private String Email;
+    private int Senha;
+
+    public CadastroUser(){
+
+    }
+
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String nome) {
+        Nome = nome;
+    }
+
+    public String getDataNascimento() {
+        return DataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        DataNascimento = dataNascimento;
+    }
+
+    public String getEmail() {
+
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public int getSenha() {
+        return Senha;
+    }
+
+    public void setSenha(int senha) {
+        this.Senha = senha;
+    }
+}
+
